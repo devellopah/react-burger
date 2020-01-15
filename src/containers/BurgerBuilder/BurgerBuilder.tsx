0d396cc2e1a28ed4bb7a4b82a0764c4141332a1a
Islam Ibakaev <islamibakaev@mail.ru>
2020-01-16 00:19:26 +0300

    added orderSummary spinner

--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,9 +1,16 @@
 import React, { Component, Fragment } from 'react';
+import { css } from "@emotion/core";
+import HashLoader from "react-spinners/HashLoader";
 import Burger from '../../components/Burger'
 import Controls from '../../components/Burger/components/Controls'
 import OrderSummary from '../../components/Burger/components/OrderSummary'
 import Modal from '../../components/ui/Modal'
 import axios from '../../axios-orders'
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -20,6 +27,7 @@ export interface IBurgerBuilderState {
   ingredients: { [k in Ingredient]: number },
   totalPrice: number,
   isPurchasing: boolean,
+  loading: boolean,
 }
 
 export default class BurgerBuilder extends Component<IBurgerBuilderProps, IBurgerBuilderState> {
@@ -33,6 +41,7 @@ export default class BurgerBuilder extends Component<IBurgerBuilderProps, IBurge
     },
     totalPrice: 0,
     isPurchasing: false,
+    loading: false,
   }
 
   addIngredient = (type :Ingredient) => {
@@ -60,6 +69,7 @@ export default class BurgerBuilder extends Component<IBurgerBuilderProps, IBurge
   }
 
   purchaseContinueHandler = () => {
+    this.setState({ loading: true })
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -75,20 +85,33 @@ export default class BurgerBuilder extends Component<IBurgerBuilderProps, IBurge
     }
     axios
       .post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
+      .then(response => {
+        this.setState({ loading: false, isPurchasing: false })
+      })
+      .catch(error => {
+        this.setState({ loading: false, isPurchasing: false })
+      })
   }
 
   public render() {
+    const orderSummary = this.state.loading
+      ? <HashLoader
+          css={override}
+          size={100}
+          color={"#8F5E1E"}
+          loading={true}
+        />
+      : <OrderSummary
+        ingredients={this.state.ingredients}
+        continued={this.purchaseContinueHandler}
+        cancelled={this.purchaseCancelHander}
+        price={this.state.totalPrice}
+      />
+
     return (
       <Fragment>
         <Modal show={this.state.isPurchasing} modalClosed={this.purchaseCancelHander}>
-          <OrderSummary
-           ingredients={this.state.ingredients}
-            continued={this.purchaseContinueHandler}
-            cancelled={this.purchaseCancelHander}
-            price={this.state.totalPrice}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <Controls
@@ -99,6 +122,6 @@ export default class BurgerBuilder extends Component<IBurgerBuilderProps, IBurge
           ordered={this.purchaseHandler}
         />
       </Fragment>
-    );
+    )
   }
 }
