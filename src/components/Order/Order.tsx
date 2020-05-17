2035251a2bb172360d6881cc64dcbc8f95cc73c9
Islam Ibakaev <islamibakaev@mail.ru>
2020-05-17 23:09:19 +0300

    finished orders page

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
 import classes from './Order.module.scss';
 
+type Ingredient = 'salad' | 'bacon' | 'cheese' | 'meat'
+
 interface IOrderProps {
+  name: string,
+  ingredients: { [k in Ingredient]: number },
+  price: number,
 }
 
 const Order: React.FunctionComponent<IOrderProps> = (props) => {
+  let ingredients = []
+  let name: Ingredient
+
+  for(name in props.ingredients) {
+    ingredients.push(
+      {
+        name,
+        amount: props.ingredients[name]
+      }
+    )
+  }
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: salad(1), meat(1)</p>
-      <p>Price: <strong>5$</strong></p>
+      <p>Name: {props.name}</p>
+      <p>Ingredients: {
+        ingredients.map((ig, i) =>
+          <span key={ig.name}>{ig.name}({ig.amount}){i < (ingredients.length - 1) && ', '}</span>
+        )}
+      </p>
+      <p>Price: <strong>{props.price.toFixed(2)}$</strong></p>
     </div>
   );
 };
--- src/containers/Orders/Orders.tsx
+++ src/containers/Orders/Orders.tsx
@@ -1,26 +1,54 @@
 import * as React from 'react';
+import axios from '../../axios-orders'
+import withError from '../../hoc/WithError'
 import Order from '../../components/Order'
 
 export interface IOrdersProps {
 }
 
 export interface IOrdersState {
+  orders: any[],
+  loading: boolean,
 }
 
-export default class Orders extends React.Component<IOrdersProps, IOrdersState> {
+class Orders extends React.Component<IOrdersProps, IOrdersState> {
   constructor(props: IOrdersProps) {
     super(props);
 
     this.state = {
+      orders: [],
+      loading: true,
     }
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const orders = []
+        for(let key in res.data) {
+          orders.push({...res.data[key], id: key})
+        }
+        this.setState({orders, loading: false})
+      })
+      .catch(err => {
+        this.setState({loading: false})
+      })
   }
 
   public render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order =>
+          <Order
+            key={order.id}
+            price={+order.price}
+            ingredients={order.ingredients}
+            name={order.customer.name}
+          />
+        )}
       </div>
     );
   }
 }
+
+export default withError(Orders, axios)
